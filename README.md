d2340240080c3c55fe63dff6819521854b11b47a
Kim Fransson <kim.fransson@sigmatechnology.se>
2023-12-23 21:10:44 +0100

    feat(project): init

---
+++ README.md
@@ -0,0 +1,7 @@
+# GifTok Project (React + daisyUI + Tailwindcss)
+
+Project designed from [BigDevSoon.me](https://bigdevsoon.me/)
+
+Live demo 
+
+## Preview
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import daisyui from "daisyui";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["nord"],
+  },
+  plugins: [
+    daisyui,
+    // https://github.com/tailwindlabs/tailwindcss-intellisense/issues/227#issuecomment-1139895799
+    ({ addUtilities }) => {
+      addUtilities({});
+    },
+  ],
+};
