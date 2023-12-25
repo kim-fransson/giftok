9f9ecb5060ea987655d4074cc9beddff0cf2c932
Kim Fransson <kim.fransson@sigmatechnology.se>
2023-12-25 22:56:53 +0100

    feat(step-8): grid display (still a bit buggy)

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import { GiphyFetch } from "@giphy/js-fetch-api";
 import { IGif } from "@giphy/js-types";
 import { useCallback, useEffect, useState } from "react";
 import { GifCard } from "./components/GifCard/GifCard";
+import { useMediaQuery } from "@uidotdev/usehooks";
+import Logo from "./assets/logo.svg?react";
 
 const GIFS_LIMIT = 20;
-const INFINITE_LOADING_THRESHOLD = 0.9;
+const INFINITE_LOADING_THRESHOLD = 0.95;
 
 export default function App() {
   const [gifs, setGifs] = useState<IGif[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const showGrid = useMediaQuery("only screen and (min-width : 768px)");
 
   const fetchAndFilterGifs = useCallback(
     async (limit = GIFS_LIMIT, offset = 0) => {
       setIsLoading(true);
       const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_API_KEY);
-
       try {
         const res = await gf.trending({ limit, offset });
         setGifs((current) => {
@@ -23,7 +25,7 @@ export default function App() {
           return updatedGifsList.filter((gif) => {
             const duplicate = seen.has(gif.id);
             seen.add(gif.id);
-            return !duplicate && gif.user;
+            return !duplicate;
           });
         });
       } catch (error) {
@@ -38,7 +40,7 @@ export default function App() {
 
   useEffect(() => {
     fetchAndFilterGifs();
-  }, [fetchAndFilterGifs]);
+  }, []);
 
   const handleIntersection = (gif: IGif) => {
     const numberOfFetchedGifs = gifs.length;
@@ -55,15 +57,45 @@ export default function App() {
   };
 
   return (
-    <div className="carousel carousel-vertical w-full h-screen">
-      {gifs.map((gif) => (
-        <div key={gif.id} className="carousel-item h-full">
-          <GifCard gif={gif} onIntersection={handleIntersection} />
+    <div className="flex flex-col h-screen">
+      <div className="navbar bg-base-100 shadow-2xl">
+        <Logo />
+      </div>
+      {showGrid ? (
+        <div>
+          <div className="max-w-7xl mx-auto p-8 md:columns-3 lg:columns-4 transition-all">
+            {gifs.map((gif) => (
+              <div key={gif.id} className="mb-5">
+                <GifCard
+                  gif={gif}
+                  onIntersection={handleIntersection}
+                  onLoading={setIsLoading}
+                />
+              </div>
+            ))}
+          </div>
+          {isLoading && (
+            <div className="flex items-center justify-center py-8">
+              <span className="loading loading-dots loading-lg"></span>
+            </div>
+          )}
         </div>
-      ))}
-      {isLoading && (
-        <div className="flex items-center justify-center py-8">
-          <span className="loading loading-dots loading-lg"></span>
+      ) : (
+        <div className="carousel carousel-vertical w-full flex-1">
+          {gifs.map((gif) => (
+            <div key={gif.id} className="carousel-item h-full">
+              <GifCard
+                gif={gif}
+                onIntersection={handleIntersection}
+                onLoading={setIsLoading}
+              />
+            </div>
+          ))}
+          {isLoading && (
+            <div className="flex items-center justify-center py-8">
+              <span className="loading loading-dots loading-lg"></span>
+            </div>
+          )}
         </div>
       )}
     </div>
--- src/components/GifCard/GifCard.tsx
+++ src/components/GifCard/GifCard.tsx
@@ -1,18 +1,22 @@
-import { useIntersectionObserver } from "@uidotdev/usehooks";
+import { useIntersectionObserver, useMediaQuery } from "@uidotdev/usehooks";
 import { useEffect, useState } from "react";
 import { IGif } from "@giphy/js-types";
 
 export type GifCardProps = {
   gif: IGif;
   onIntersection: (gif: IGif) => void;
+  onLoading: (value: boolean) => void;
 };
 
 export const GifCard = (props: GifCardProps) => {
-  const { gif, onIntersection } = props;
+  const { gif, onIntersection, onLoading } = props;
   const user = gif.user;
 
   const [isLoadingGif, setIsLoadingGif] = useState(true);
+
   const [isLoadingUserAvatar, setIsLoadingUserAvatar] = useState(true);
+
+  const useIntersection = useMediaQuery("only screen and (min-width : 768px)");
 
   const [ref, entry] = useIntersectionObserver({
     threshold: 0,
@@ -21,51 +25,63 @@ export const GifCard = (props: GifCardProps) => {
   });
 
   useEffect(() => {
+    onLoading(isLoadingGif);
+  }, [isLoadingGif, onLoading]);
+
+  useEffect(() => {
     if (entry?.isIntersecting) {
       onIntersection(gif);
     }
   }, [entry, gif, onIntersection]);
 
   return (
-    <div ref={ref} className="flex flex-col gap-4 w-full">
-      {entry?.isIntersecting && (
+    <div
+      ref={ref}
+      className="flex flex-col gap-4 w-full md:rounded-lg md:overflow-hidden md:shadow-2xl relative group"
+    >
+      {(entry?.isIntersecting || useIntersection) && (
         <img
-          className={`my-auto w-full ${
+          className={`md:my-0 my-auto md:w-auto w-full ${
             isLoadingGif && "skeleton rounded-none"
           }`}
           height={gif.images.original.height}
           width={gif.images.original.width}
           src={gif.images.original.url}
-          onLoad={() => setIsLoadingGif(false)}
+          onLoad={() => {
+            setIsLoadingGif(false);
+          }}
         />
       )}
-
-      {user && (
-        <div className="flex flex-col gap-1 p-4">
-          <a
-            className="flex gap-2 items-center"
-            href={`https://giphy.com/${user.username}/`}
-            target="_blank"
-          >
-            <div className="avatar">
-              <div
-                className={`w-8 rounded ${isLoadingUserAvatar && "skeleton"}`}
-              >
-                {entry?.isIntersecting && (
-                  <img
-                    src={user.avatar_url}
-                    onLoad={() => setIsLoadingUserAvatar(false)}
-                  />
-                )}
+      <div className="flex flex-col gap-1 p-4 md:hidden group-hover:flex md:absolute md:bottom-4 md:left-4 md:right-4 md:bg-base-100 md:rounded-lg">
+        {user ? (
+          <>
+            <a
+              className="flex gap-2 items-center"
+              href={`https://giphy.com/${user.username}/`}
+              target="_blank"
+            >
+              <div className="avatar">
+                <div
+                  className={`w-8 rounded ${isLoadingUserAvatar && "skeleton"}`}
+                >
+                  {entry?.isIntersecting && (
+                    <img
+                      src={user.avatar_url}
+                      onLoad={() => setIsLoadingUserAvatar(false)}
+                    />
+                  )}
+                </div>
               </div>
-            </div>
-            <span className="font-medium text-primary">
-              {user.display_name}
-            </span>
-          </a>
-          <span>{gif.title}</span>
-        </div>
-      )}
+              <span className="font-medium text-primary">
+                {user.display_name}
+              </span>
+            </a>
+            <span>{gif.title}</span>
+          </>
+        ) : (
+          <span>No account, no problem! This GIF flies solo. 🤷‍♂️</span>
+        )}
+      </div>
     </div>
   );
 };
