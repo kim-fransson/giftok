bdde0198234227cba772ec9150d6d6a94041c009
Kim Fransson <kim.fransson@sigmatechnology.se>
2023-12-25 13:50:54 +0100

    feat(step-5): infinite scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,65 @@
-import { GifsResult, GiphyFetch } from "@giphy/js-fetch-api";
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { IGif } from "@giphy/js-types";
 import { useEffect, useState } from "react";
 import { GifCard } from "./components/GifCard/GifCard";
 
+const GIFS_LIMIT = 20;
+const INFINITE_LOADING_THRESHOLD = 0.7;
+
 export default function App() {
-  const [gifs, setGifs] = useState<GifsResult>();
+  const [gifs, setGifs] = useState<IGif[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
+  const fetchAndFilterGifs = async (limit = GIFS_LIMIT, offset = 0) => {
     setIsLoading(true);
     const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_API_KEY);
-    gf.trending({ limit: 20 }).then((res) => {
-      setGifs(res);
+
+    try {
+      const res = await gf.trending({ limit, offset });
+      setGifs((current) => {
+        const seen = new Set();
+        const updatedGifsList = [...current, ...res.data];
+        return updatedGifsList.filter((gif) => {
+          const duplicate = seen.has(gif.id);
+          seen.add(gif.id);
+          return !duplicate;
+        });
+      });
+    } catch (error) {
+      console.error("Error fetching GIFs:", error);
+      // todo: handle errors
+    } finally {
       setIsLoading(false);
-    });
+    }
+  };
+
+  useEffect(() => {
+    fetchAndFilterGifs();
   }, []);
+
+  const handleIntersection = (gif: IGif) => {
+    const numberOfFetchedGifs = gifs.length;
+
+    const indexOfIntersectedGifs = Array.from(gifs).indexOf(gif);
+
+    const shouldLoadMoreGifs =
+      (indexOfIntersectedGifs + 1) / numberOfFetchedGifs >
+      INFINITE_LOADING_THRESHOLD;
+
+    if (shouldLoadMoreGifs) {
+      fetchAndFilterGifs(GIFS_LIMIT, numberOfFetchedGifs);
+    }
+  };
 
   return (
     gifs && (
       <div className="h-screen carousel carousel-vertical">
-        {gifs.data.map((gif) => (
-          <div className="carousel-item h-full">
+        {gifs.map((gif) => (
+          <div key={gif.id} className="carousel-item h-full">
             <GifCard
-              key={gif.id}
-              url={gif.images.original.url}
-              alt={gif.alt_text || "gif"}
+              gif={gif}
               isLoading={isLoading}
+              onIntersection={handleIntersection}
             />
           </div>
         ))}
--- src/components/GifCard/GifCard.tsx
+++ src/components/GifCard/GifCard.tsx
@@ -1,18 +1,37 @@
+import { useIntersectionObserver } from "@uidotdev/usehooks";
+import { useEffect } from "react";
+import { IGif } from "@giphy/js-types";
+
 export type GifCardProps = {
-  url: string;
-  alt: string;
+  gif: IGif;
   isLoading: boolean;
+  onIntersection: (gif: IGif) => void;
 };
 
 export const GifCard = (props: GifCardProps) => {
+  const { gif, isLoading, onIntersection } = props;
+
+  const [ref, entry] = useIntersectionObserver({
+    threshold: 0,
+    root: null,
+    rootMargin: "0px",
+  });
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      console.log(`${gif.id} is intersecting`);
+      onIntersection(gif);
+    }
+  }, [entry, gif, onIntersection]);
+
   return (
-    <div
-      className={`flex items-center justify-center w-full ${
-        props.isLoading && "skeleton"
-      }`}
-    >
-      <figure className="w-full">
-        <img className="w-full h-full" src={props.url} alt={props.alt} />
+    <div ref={ref} className={`flex items-center w-full`}>
+      <figure className="flex-1">
+        <img
+          className="w-full h-full"
+          src={gif.images.original.url}
+          alt={gif.alt_text}
+        />
       </figure>
     </div>
   );
